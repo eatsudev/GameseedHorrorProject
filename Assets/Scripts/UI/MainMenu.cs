da18ee4093ad4122b6e1dfb6e7e93b054d3c4850
eatsudev <wizardbolt01@gmail.com>
2024-09-02 16:14:04 +0700

    dddd

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;
+    public Button quitButton;
+    public Button optionsButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(PlayButton);
+        quitButton.onClick.AddListener(QuitButton);
+        optionsButton.onClick.AddListener(OptionsButton);
+    }
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Opening");
+        Time.timeScale = 1.0f;
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    
+}
