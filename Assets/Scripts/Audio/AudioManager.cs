dba006633c5130bc9b1ef5db16ef4170fafeeb67
eatsudev <wizardbolt01@gmail.com>
2024-09-10 19:40:15 +0700

    audio asset, audio manager fix

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,34 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     public AudioMixer masterMixer;
+    public Slider masterSlider;
     public Slider bgmSlider;
     public Slider sfxSlider;
+
+    private float masterValue;
+    private float bgmValue;
+    private float sfxValue;
     private void Start()
     {
-        bgmSlider.value = PlayerPrefs.GetFloat("BGMVolume", 0.75f);
-        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0.75f);
+        bgmSlider.value = PlayerPrefs.GetFloat("BGM", 0.75f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFX", 0.75f);
         
+        SetMasterVolume(masterSlider.value);
         SetBGMVolume(bgmSlider.value);
         SetSFXVolume(sfxSlider.value);
 
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
         bgmSlider.onValueChanged.AddListener(SetBGMVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
+    public void SetMasterVolume(float volume)
+    {
+        masterMixer.SetFloat("MasterVolume", ValueToVolume(volume, 80));
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+
+        masterValue = volume;
+    }
+
     public void SetBGMVolume(float volume)
     {
-        masterMixer.SetFloat("BGMVolume", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("BGMVolume", volume);
+        masterMixer.SetFloat("BGM", ValueToVolume(volume, masterValue * 80));
+        PlayerPrefs.SetFloat("BGM", volume);
+
+        bgmValue = volume;
     }
 
     public void SetSFXVolume(float volume)
     {
-        masterMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("BGMVolume", volume);
+        masterMixer.SetFloat("SFX", ValueToVolume(volume, masterValue * 80));
+        PlayerPrefs.SetFloat("SFX", volume);
+
+        sfxValue = volume;
     }
+    private float ValueToVolume(float value, float maxVolume)
+    {
+        return Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * maxVolume;
+    }   
 }
