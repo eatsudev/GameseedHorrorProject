792114ebe741d9e00bf9c5b6710e19de591f7842
eatsudev <wizardbolt01@gmail.com>
2024-09-11 11:56:42 +0700

    ui pause assets, fix mouse look y axis, save volume func (fix otw)

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -11,29 +11,43 @@ public class AudioManager : MonoBehaviour
     public Slider masterSlider;
     public Slider bgmSlider;
     public Slider sfxSlider;
+    public Button saveButton;
+    public Button discardButton;
+    public Button resetButton;
 
     private float masterValue;
     private float bgmValue;
     private float sfxValue;
+
+    private float defaultVolume = 0.75f;
     private void Start()
     {
-        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0.75f);
-        bgmSlider.value = PlayerPrefs.GetFloat("BGM", 0.75f);
-        sfxSlider.value = PlayerPrefs.GetFloat("SFX", 0.75f);
-        
-        SetMasterVolume(masterSlider.value);
-        SetBGMVolume(bgmSlider.value);
-        SetSFXVolume(sfxSlider.value);
+        LoadVolumeSettings();
 
         masterSlider.onValueChanged.AddListener(SetMasterVolume);
         bgmSlider.onValueChanged.AddListener(SetBGMVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+
+        saveButton.onClick.AddListener(SaveVolumeSettings);
+        discardButton.onClick.AddListener(DiscardVolumeSettings);
+        resetButton.onClick.AddListener(ResetVolumeSettings);
+    }
+
+    private void LoadVolumeSettings()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", defaultVolume);
+        bgmSlider.value = PlayerPrefs.GetFloat("BGM", defaultVolume);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFX", defaultVolume);
+
+        SetMasterVolume(masterSlider.value);
+        SetBGMVolume(bgmSlider.value);
+        SetSFXVolume(sfxSlider.value);
     }
 
     public void SetMasterVolume(float volume)
     {
         masterMixer.SetFloat("MasterVolume", ValueToVolume(volume, 80));
-        PlayerPrefs.SetFloat("MasterVolume", volume);
+        //PlayerPrefs.SetFloat("MasterVolume", volume);
 
         masterValue = volume;
     }
@@ -41,7 +55,7 @@ public class AudioManager : MonoBehaviour
     public void SetBGMVolume(float volume)
     {
         masterMixer.SetFloat("BGM", ValueToVolume(volume, masterValue * 80));
-        PlayerPrefs.SetFloat("BGM", volume);
+        //PlayerPrefs.SetFloat("BGM", volume);
 
         bgmValue = volume;
     }
@@ -49,7 +63,7 @@ public class AudioManager : MonoBehaviour
     public void SetSFXVolume(float volume)
     {
         masterMixer.SetFloat("SFX", ValueToVolume(volume, masterValue * 80));
-        PlayerPrefs.SetFloat("SFX", volume);
+        //PlayerPrefs.SetFloat("SFX", volume);
 
         sfxValue = volume;
     }
@@ -57,4 +71,37 @@ public class AudioManager : MonoBehaviour
     {
         return Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * maxVolume;
     }   
+
+    public void SaveVolumeSettings()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", masterSlider.value);
+        PlayerPrefs.SetFloat("BGMVolume", bgmSlider.value);
+        PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
+        PlayerPrefs.Save();
+        Debug.Log("Volume Settings Saved");
+    }
+
+    public void DiscardVolumeSettings()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", defaultVolume);
+        bgmSlider.value = PlayerPrefs.GetFloat("BGMVolume", defaultVolume);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", defaultVolume);
+
+        SetMasterVolume(masterSlider.value);
+        SetBGMVolume(bgmSlider.value);
+        SetSFXVolume(sfxSlider.value);
+        Debug.Log("Volume settings discarded");
+    }
+
+    public void ResetVolumeSettings()
+    {
+        masterSlider.value = defaultVolume;
+        bgmSlider.value = defaultVolume;
+        sfxSlider.value = defaultVolume;
+
+        SetMasterVolume(defaultVolume);
+        SetBGMVolume(defaultVolume);
+        SetSFXVolume(defaultVolume);
+        Debug.Log("Volume settings has been reset to default");
+    }
 }
